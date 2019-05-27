4c93c73c9748818bbe93714f8ddb924125edeecf
AntonVoronezh <pstut@mail.ru>
2019-05-27 21:22:28 +0300

    axios.js

--- src/helpers/axios/axios.js
+++ src/helpers/axios/axios.js
@@ -1,8 +1,17 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const url = 'https://newsapi.org/v2/'
-const apiKey = '7fd45e35dc9349c8ad721a77101089d6'
+const url = 'https://newsapi.org/v2/';
+const apiKey = '7fd45e35dc9349c8ad721a77101089d6';
 
 export default () => {
-   return axios.get(`${url}top-headlines?sources=bbc-news&apiKey=${apiKey}`)
-}
+	return axios.get(`${url}top-headlines?sources=bbc-news&apiKey=${apiKey}`);
+};
+
+export const tryLogin = controls => {
+	const { login, password } = controls;
+	return axios.post('https://mysterious-reef-29460.herokuapp.com/api/v1/validate', {
+		email: login,
+      password,
+      // content-type: application/json
+	});
+};
