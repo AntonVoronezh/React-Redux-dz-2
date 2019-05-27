0b35c406b6af80cab45c55d1aa2ddcaae80fe27e
AntonVoronezh <pstut@mail.ru>
2019-05-27 21:23:08 +0300

    add News.js

--- src/containers/pages/News.js
+++ src/containers/pages/News.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import News from '../../components/pages/News';
-import getNews from '../../helpers/axios/axios';
 import fetchNews from '../../store/actions/newsActions';
 
 class NewsContainer extends React.Component {
